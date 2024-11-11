d0e2013097fd5e24361b3ad8705e6864e3bbe7f9
Ana <157633210+adono-ma@users.noreply.github.com>
2024-11-11 19:12:01 +0000

    no sé ni lo que he hecho, pero no funciona

---
+++ Hell.java
@@ -0,0 +1,8 @@
+public class Hell {
+    public static void main(String[] args) {
+        String hell;
+        hell = "Hello World";
+        System.out.println(hell);
+    }
+    
+}
---
+++ Hello_World.java
@@ -0,0 +1,6 @@
+public class Hello_World { 
+    public static void main(String[] args) {
+        String hello = "Hello World";
+        System.out.println(hello);
+    }
+}
